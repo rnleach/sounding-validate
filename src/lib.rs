14e3c64dd7437c6e91ac9d6c80509b01b6b2b772
Ryan <rnleach@users.noreply.github.com>
2018-03-27 13:53:16 -0600

    Link example in readme.

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 /*! Provides validation facilities for the 
 [sounding-base](https://github.com/rnleach/sounding-base.git) crate.
 
+See [integration tests](tests/validation_test.rs) for example of library use.
+
 */
 
 //
