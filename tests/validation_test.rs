4cade0783871b9c264e10283c5de3fe6ff768245
Ryan <rnleach@users.noreply.github.com>
2018-04-02 16:13:56 -0600

    Upgrade to 0.5. (#5)
    * Appveyor: Closes #3 by adding appveyor CI.
    * Upgrade to sounding-base 0.4.0 Got it compiling again.
    * Link example in readme.
    * Add examples. Fixes #2.
    * Fixes #1, fixes clippy lints, and all tests passing.
    * Better organization.
    * Updating for sounding-base v0.5.

--- tests/validation_test.rs
+++ tests/validation_test.rs
@@ -137,7 +137,12 @@ fn test_pressure_not_decreasing_with_height() {
     let snd = create_invalid_test_sounding_pressure_not_decreasing_with_height();
     let result = validate(&snd);
     assert!(result.is_err());
-    assert!(result.unwrap_err().to_inner()[0] == ValidationError::PressureNotDecreasingWithHeight);
+    assert!(
+        result
+            .unwrap_err()
+            .to_inner()
+            .contains(&ValidationError::PressureNotDecreasingWithHeight)
+    );
 }
 
 fn create_invalid_test_sounding_pressure_not_decreasing_with_height() -> Sounding {
@@ -148,8 +153,11 @@ fn create_invalid_test_sounding_pressure_not_decreasing_with_height() -> Soundin
 fn test_no_pressure_profile() {
     let snd = create_invalid_test_sounding_no_pressure_profile();
     let result = validate(&snd);
-    assert!(result.is_err());
-    assert!(result.unwrap_err().to_inner()[0] == ValidationError::NoPressureProfile);
+
+    let err = result.unwrap_err();
+    println!("{}", err);
+
+    assert!(err.to_inner().contains(&ValidationError::NoPressureProfile));
 }
 
 fn create_invalid_test_sounding_no_pressure_profile() -> Sounding {
@@ -167,7 +175,7 @@ fn test_no_invalid_vector_length() {
         err.to_inner()[0]
     {
         assert!(desc == "Temperature");
-        assert!(actual_length == 7 && desired_length == 8);
+        assert!(actual_length == 8 && desired_length == 9);
     } else {
         panic!("Error is of wrong type!");
     }
@@ -204,6 +212,7 @@ fn test_temperature_less_than_wetbulb() {
             panic!("Error is of wrong type!");
         }
     }
+
 }
 
 fn create_invalid_test_temperature_less_than_wetbulb() -> Sounding {
@@ -299,7 +308,7 @@ fn test_invalid_negative_value() {
     println!("{}", err);
 
     let errs = err.to_inner();
-    assert!(errs.len() == 8);
+    assert!(errs.len() == 9);
 
     for err in errs {
         if let ValidationError::InvalidNegativeValue(_, val) = err {
@@ -348,7 +357,7 @@ fn test_invalid_wind_direction() {
     println!("{}", err);
 
     let errs = err.to_inner();
-    assert!(errs.len() == 5);
+    assert!(errs.len() == 6);
 
     for err in errs {
         if let ValidationError::InvalidWindDirection(val) = err {
